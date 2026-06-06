0d351436a4b4b8956c8f99e87219f2f60ee03cdd
sannichauhan <chauhansanni94@gmail.com>
2026-06-06 20:08:19 +0530

    domain name updated

--- school_app/settings.py
+++ school_app/settings.py
@@ -24,9 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_mhg(98qj(+_ks!5@yy*c0hdcjn!5xkv(g04gy&@)ibii()joz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "navchetanapublicschool.com",
+    "www.navchetanapublicschool.com"
+]
 
 
 # Application definition
