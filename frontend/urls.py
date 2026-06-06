5523972620758306500e761796e22843a4baf915
sannichauhan <chauhansanni94@gmail.com>
2026-06-06 20:29:49 +0530

    home page updated

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import main_home_page
 
 urlpatterns = [
-    path('', views.home_page, name="home-page"),
+    path('', main_home_page, name="homepage"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
-def home_page(request):
-    return HttpResponse("Django is working")
 
+def main_home_page(request):
+    return render(request, 'home.html')
