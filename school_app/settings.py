47a3c8ce97addafdcdb5846d6b9022dd1de5035a
sannichauhan <chauhansanni94@gmail.com>
2026-06-12 13:38:37 +0530

    sqllite replace with mysql

--- school_app/settings.py
+++ school_app/settings.py
@@ -101,10 +101,21 @@ WSGI_APPLICATION = "school_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'navcheta_ncps_db',
+        'USER': 'navcheta_ncps_user',
+        'PASSWORD': 'OW)!nY3aT0NsPb@!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
